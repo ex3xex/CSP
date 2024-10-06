0a23baab446ae8dcd319a3dac2308e5532f40cb6
ex3xex <68012861+ex3xex@users.noreply.github.com>
2024-10-06 17:15:17 +0900

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,4 +1,4 @@
-fetch('http://localhost/server-status')
+fetch('/robots.txt')
   .then(response => response.text())
   .then(text => alert(text))
-  .catch(error => alert('SSRF Failed: ' + error.message));
+  .catch(error => alert('LFI Failed: ' + error.message));
